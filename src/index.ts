9bb91cce75bd4ab469dea671f4c12c6b018b777e
charlesLoder <charles.w.loder@gmail.com>
2021-06-22 18:09:46 -0400

    Update package info

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,3 @@
 import { Text } from "./text";
 
 export { Text };
-
-// add back to package.json
-// "repository": {
-//     "type": "git",
-//     "url": "https://github.com/charlesLoder/umschrift.git"
-//   },
-//   "bugs": {
-//     "url": "https://github.com/charlesLoder/umschrift/issues"
-//   },
-//   "homepage": "https://github.com/charlesLoder/umschrift"
